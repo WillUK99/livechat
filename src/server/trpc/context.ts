e4638f6ea06fc387951bc64dab86944ea196b4cf
WillUK99 <76099444+WillUK99@users.noreply.github.com>
2022-10-31 08:41:52 +0000

    fix: trpc context types*

--- src/server/trpc/context.ts
+++ src/server/trpc/context.ts
@@ -1,13 +1,17 @@
 // src/server/router/context.ts
-import ws from 'ws'
+import type ws from 'ws'
 import type { inferAsyncReturnType } from "@trpc/server";
 import type { CreateNextContextOptions } from "@trpc/server/adapters/next";
 import type { Session } from "next-auth";
 import { EventEmitter } from 'events';
-import { getServerAuthSession } from "../common/get-server-auth-session";
 import { prisma } from "../db/client";
-import { NodeHTTPCreateContextFnOptions, NodeHTTPHandlerOptions } from '@trpc/server/dist/adapters/node-http';
+import { NodeHTTPCreateContextFnOptions } from '@trpc/server/dist/adapters/node-http';
 import { IncomingMessage } from 'http';
+import { getSession } from 'next-auth/react';
+
+type CreateContextOptions = {
+  session: Session | null;
+}
 
 const eventEmitter = new EventEmitter()
 
@@ -15,7 +19,7 @@ const eventEmitter = new EventEmitter()
  * - testing, so we dont have to mock Next.js' req/res
  * - trpc's `createSSGHelpers` where we don't have req/res
  **/
-export const createContextInner = async (opts: NodeHTTPCreateContextFnOptions<IncomingMessage, ws> & { session: Session | null }) => {
+export const createContextInner = async (opts: CreateContextOptions) => {
   return {
     session: opts.session,
     prisma,
@@ -27,11 +31,12 @@ export const createContextInner = async (opts: NodeHTTPCreateContextFnOptions<In
  * This is the actual context you'll use in your router
  * @link https://trpc.io/docs/context
  **/
-export const createContext = async (opts: CreateNextContextOptions) => {
-  const { req, res } = opts;
+export const createContext = async (opts?: | CreateNextContextOptions | NodeHTTPCreateContextFnOptions<IncomingMessage, ws>) => {
+  const req = opts?.req
+  const res = opts?.res
 
   // Get the session from the server using the unstable_getServerSession wrapper function
-  const session = await getServerAuthSession({ req, res });
+  const session = await getSession({ req });
 
   return await createContextInner({
     session,
